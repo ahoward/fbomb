1c9e479799fe52a921515638e7ec9b15dd00e79c
ara.t.howard <ara.t.howard@gmail.com>
2011-09-18 10:31:02 -0600

    mo betta fbomb

--- lib/fbomb/commands/system.rb
+++ lib/fbomb/commands/system.rb
@@ -17,7 +17,12 @@ FBomb {
 
   command(:fbomb) {
     call {
-      speak('http://s3.amazonaws.com/drawohara.com.mp3/tom_jones_sex_bomb_dance_remix.mp3')
+      urls = %w(
+        http://s3.amazonaws.com/drawohara.com.mp3/tom_jones_sex_bomb_dance_remix.mp3
+        http://4.bp.blogspot.com/-K7nKv-g9WyQ/Thb7Jqw-YoI/AAAAAAAABeo/e0AWFySD_GY/s1600/Tom+Jones+2.jpg
+        http://www.fitceleb.com/files/tom_jones.jpg
+      )
+      speak(urls.sort_by{ rand }.first)
     }
   }
 }
