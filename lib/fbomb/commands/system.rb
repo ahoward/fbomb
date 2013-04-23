436b2dff4fefc2faa8b27494173f9248a3a41524
Frederic Jean <fred@fredjean.net>
2013-04-23 16:56:43 -0600

    Adding a small easter egg...

--- lib/fbomb/commands/system.rb
+++ lib/fbomb/commands/system.rb
@@ -1,23 +1,27 @@
 FBomb {
   command(:help) {
     call do |*args|
-      sections = []
-      Command.table.each do |path, command|
-        next if path == '/help'
-        help = command.help || path
-        chunk = [path, Util.indent(help) + "\n"]
-        sections.push(chunk)
+      if args.join(' ') =~ /me/i
+        speak("You are beyond help...")
+      else
+        sections = []
+        Command.table.each do |path, command|
+          next if path == '/help'
+          help = command.help || path
+          chunk = [path, Util.indent(help) + "\n"]
+          sections.push(chunk)
+        end
+        sections.sort!{|a, b| a.first <=> b.first}
+        sections.push(["/help", Util.indent("this message") + "\n"])
+        msg = sections.join("\n")
+        paste(msg) unless msg.strip.empty?
       end
-      sections.sort!{|a, b| a.first <=> b.first}
-      sections.push(["/help", Util.indent("this message") + "\n"])
-      msg = sections.join("\n")
-      paste(msg) unless msg.strip.empty?
     end
   }
 
   command(:reload){
     help 'reload fbomb commands'
-    
+
     call do |*args|
       #Thread.critical = true
       table = FBomb::Command.table
